c6703f6b7e4c4948776df5147eb3fcd9d62723f7
zhonglunsheng” <873946453@qq.com>
2018-03-09 20:22:26 +0800

    ''

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
     private final static String COOKIE_NAME = "mmall_login_token";
-    private final static String COOKIE_DOMAIN = ".weshop.com";
+    private final static String COOKIE_DOMAIN = ".i20forever.cn";
 
     public static String readLoginToken(HttpServletRequest request){
         Cookie[] cks = request.getCookies();
